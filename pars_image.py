755f066c5325eb540ee58db718864d786da03fac
Boaruzhanchik <134432508+Boaruzhanchik@users.noreply.github.com>
2023-10-26 19:32:57 +0300

    Update pars_image.py

--- pars_image.py
+++ pars_image.py
@@ -9,20 +9,16 @@ import os
 
 driver = webdriver.Chrome()
 
-# Перейдите на веб-сайт, с которого вы хотите сохранить фотографии
 driver.get("https://fishcanal.ru/catalog/udilishcha/")
 
-wait = WebDriverWait(driver, 10)  # Ожидание в течение 10 секунд
+wait = WebDriverWait(driver, 10)
 wait.until(EC.visibility_of_element_located((By.TAG_NAME, "img")))
 
-# Найти все элементы с изображениями на странице (пример: элементы img)
 image_elements = driver.find_elements(By.TAG_NAME, "img")
 
-# Создайте папку для сохранения изображений
 if not os.path.exists("images"):
     os.makedirs("images")
 
-# Итерируйтесь по всем найденным элементам с изображениями
 for index, image_element in enumerate(image_elements):
     image_url = image_element.get_attribute("src")
 
@@ -36,5 +32,4 @@ for index, image_element in enumerate(image_elements):
         except Exception as e:
             print(f"Ошибка при сохранении изображения: {str(e)}")
 
-# Закройте браузер
 driver.quit()
